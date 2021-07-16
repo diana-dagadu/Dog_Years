9eb84b6ba0b287d71f057c91f213a67c656d1de9
diana-dagadu <diana.dagadu@amalitech.org>
2021-07-16 16:45:28 +0000

    creted a simple script file that calculates age in dogYears

---
+++ app.js
@@ -0,0 +1,21 @@
+// declare an age variable 
+const myAge = 22;
+
+// create a variable earlyYears
+let earlyYears = 2;
+
+// create a variable earlyYears
+earlyYears = earlyYears * 10.5;
+
+// create a variable laterYears
+let laterYears = myAge - 2;
+
+laterYears *= 4;
+
+// create a variable that adds earlyYears and laterYears
+const myAgeInDogYears = earlyYears + laterYears;
+
+// declare a variable that stores my name in lowercase
+let myName = 'Diana'.toLowerCase();
+
+console.log(`My name is ${myName}. I am ${myAge}years old in human years which is ${myAgeInDogYears} years old in dog years.`);
